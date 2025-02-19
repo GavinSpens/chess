8db9d16572d5ca88159fa2a25108c4e008dd1359
Gavin Spens <gavinspens@gmail.com>
2025-02-19 15:58:13 -0700

    starting phase 3

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,21 @@
+package model;
+
+import chess.ChessGame;
+
+class GameData {
+    private int gameID;
+    private String whiteUsername;
+    private String blackUsername;
+    private String gameName;
+    private ChessGame game;
+
+    GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+
+}
