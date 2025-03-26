466c5226494897abbf0034691bd71e485d3f3b65
Gavin Spens <gavinspens@gmail.com>
2025-03-26 00:18:16 -0600

    done

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,14 +2,14 @@
 import ui.Repl;
 
 public class Main {
-    private static final int port = 8080;
-    private static final String serverUrl = "http://localhost:" + port;
+    private static final int PORT = 8080;
+    private static final String SERVER_URL = "http://localhost:" + PORT;
 
     public static void main(String[] args) {
 //        Server server = new Server();
 //        server.run(port);
 
-        Repl repl = new Repl(serverUrl);
+        Repl repl = new Repl(SERVER_URL);
         repl.run();
     }
 }
--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -6,7 +6,7 @@ import chess.ChessGame;
 import chess.ChessPiece;
 import exception.ResponseException;
 import model.*;
-import server.ServerFacade;
+import serverFacade.ServerFacade;
 
 public class ChessClient {
     private UserData userData = null;
